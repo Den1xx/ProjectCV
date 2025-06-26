877f92a3db336f2826144118e26f6141ecd968cf
Den1xx <denhis_1905@hotmail.com>
2025-06-26 15:52:13 +0300

    ViewComponent

---
+++ ProjectCV.WEBUI/ViewComponents/UserProfile/_SelectUserProfileViewComponentPartial.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectCV.BLL.Concrete;
+using ProjectCV.ENTITY.Entities;
+
+namespace ProjectCV.WEBUI.ViewComponents.UserProfile
+{
+    public class _SelectUserProfileViewComponentPartial : ViewComponent
+    {
+        private readonly UserProfileService _userProfileService;
+        public _SelectUserProfileViewComponentPartial()
+        {
+            _userProfileService = new UserProfileService();
+        }
+        public IViewComponentResult Invoke()
+        {
+            var userProfile = _userProfileService.GetUserProfile();
+            return View(userProfile);
+        }
+    }
+}
