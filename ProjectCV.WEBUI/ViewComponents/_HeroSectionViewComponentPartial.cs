1fcedfe470dec197d22b4b1ca5add01657d0f5ef
Den1xx <denhis_1905@hotmail.com>
2025-06-30 16:06:06 +0300

    HeroSection Added

---
+++ ProjectCV.WEBUI/ViewComponents/_HeroSectionViewComponentPartial.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectCV.BLL;
+
+namespace ProjectCV.WEBUI.ViewComponents
+{
+    public class _HeroSectionViewComponentPartial : ViewComponent
+    {
+        private readonly UserProfileService userProfileService;
+        public _HeroSectionViewComponentPartial()
+        {
+            userProfileService = new UserProfileService();
+        }
+        public IViewComponentResult Invoke()
+        {
+            var hero = userProfileService.GetUserProfile();
+            return View(hero);
+        }
+    }
+}
