dec71b99456c83355bb478a4f82cc35f56478433
Den1xx <denhis_1905@hotmail.com>
2025-07-24 19:16:04 +0300

    homecontroller

--- ProjectCV.WEBUI/Controllers/HomeController.cs
+++ ProjectCV.WEBUI/Controllers/HomeController.cs
@@ -1,12 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ProjectCV.BLL;
+using System.Threading.Tasks;
 
 namespace ProjectCV.Controllers
 {
     public class HomeController : Controller    {
         
-        public IActionResult Index()
+        private readonly HttpClient client = new HttpClient();
+        public async Task<IActionResult> Index()
         {
+            HttpResponseMessage response = await client.GetAsync("https://localhost:7201/api/Skills");
             return View();
         }
     }
