e81f126f0a451babbe732df7d446482e2a3241a5
Den1xx <denhis_1905@hotmail.com>
2025-08-25 15:16:37 +0300

    Dto created

---
+++ ProjectCV.DAL/DTOs/UserProfileDto/UserProfileUpdateDTO.cs
@@ -0,0 +1,20 @@
+﻿using ProjectCV.ENTITY.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectCV.DAL.DTOs.UserProfileDto
+{
+    public class UserProfileUpdateDTO
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string ProfileImage { get; set; }
+        public string BackgroundImage { get; set; }
+
+        public List<SocialMedia> SocialMedias { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
---
+++ ProjectCV.DAL/Mapping/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using ProjectCV.DAL.DTOs.UserProfileDto;
+using ProjectCV.ENTITY.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectCV.DAL.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<UserProfileUpdateDTO,UserProfile>().ReverseMap();
+        }
+    }
+}
